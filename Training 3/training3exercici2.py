ad6a5f6317fb4f5c258fd5f2ccc5804c0d883d98
Raúl Martis Hernández <raulmartis@iesmontsia.org>
2025-11-06 17:39:04 +0100

    commit3

--- Training 3/training3exercici2.py
+++ Training 3/training3exercici2.py
@@ -2,9 +2,9 @@
 num1 = float(input("Introdueix el primer nombre: "))  # Demanem el primer nombre
 num2 = float(input("Introdueix el segon nombre: "))   # Demanem el segon nombre
 num3 = float(input("Introdueix el tercer nombre: "))   # Demanem el tercer nombre
-if num1 > num2 and num1 > num3:
+if num1 > num2 and num1 > num3: # amb (if and) podem afegir més d'una condició i ens facilita comparar més valors
     print("El nombre major és:", num1)  # Mostrem el primer nombre si és el major
-elif num2 > num1 and num2 > num3:
+elif num2 > num1 and num2 > num3: # Comprovem si el segon nombre és major que els altres dos
     print("El nombre major és:", num2)  # Mostrem el segon nombre si és el major
 else:
     print("El nombre major és:", num3)  # Mostrem el tercer nombre si és el major
--- Training 3/training3exercici3.py
+++ Training 3/training3exercici3.py
@@ -1,7 +1,7 @@
 num = float(input("Introdueix un nombre: ")) # Demanem a l'usuari que introdueixi un nombre
-if num >= 0:
+if num >= 0: # Comprovem si el nombre és positiu o negatiu amb la condició (if else)
     print("El nombre és positiu.")
-else:
-    print("El nombre és negatiu.")
-
+else: # Si no és positiu, és negatiu
+    print("El nombre és negatiu.") # Mostrem missatge si el nombre és negatiu
+    
     
