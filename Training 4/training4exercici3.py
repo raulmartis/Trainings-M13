6065a3d5054204bda28c9c02f4b9d46f7e8684fc
Raúl Martis Hernández <raulmartis@iesmontsia.org>
2025-11-07 17:41:19 +0100

    add file modified

--- Training 4/training4exercici3.py
+++ Training 4/training4exercici3.py
@@ -1,6 +1,6 @@
 nombre_negatiu = False
 
-for _ in range(10):
+for i in range(10):
     nombre = int(input("Introdueix un nombre: "))
     if nombre < 0:
         nombre_negatiu = True
@@ -9,5 +9,5 @@ if nombre_negatiu:
     print("Hi havia almenys un nombre negatiu")
 else:
     print("No hi ha cap nombre negatiu")
-7
+
 
