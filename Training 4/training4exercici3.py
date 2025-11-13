6ccab9b8f7b7a77767f98bfed82d6ce92dd652d7
Raúl Martis Hernández <raulmartis@iesmontsia.org>
2025-11-13 15:10:39 +0100

    add file modifiede

--- Training 4/training4exercici3.py
+++ Training 4/training4exercici3.py
@@ -1,13 +1,13 @@
-nombre_negatiu = False
+nombre_negatiu = False # Variable per rastrejar si hi ha algun nombre negatiu
 
-for i in range(10):
-    nombre = int(input("Introdueix un nombre: "))
-    if nombre < 0:
-        nombre_negatiu = True
+for i in range(10): # Bucle per demanar 10 nombres a l'usuari
+    nombre = int(input("Introdueix un nombre: ")) # Sol·licita un nombre a l'usuari
+    if nombre < 0: # Comprova si el nombre és negatiu
+        nombre_negatiu = True # Actualitza la variable si hi ha un nombre negatiu
 
-if nombre_negatiu:
-    print("Hi havia almenys un nombre negatiu")
-else:
-    print("No hi ha cap nombre negatiu")
+if nombre_negatiu: # Comprova si hi ha hagut algun nombre negatiu
+    print("Hi havia almenys un nombre negatiu") # Imprimeix el missatge corresponent
+else:   # Si no hi ha hagut cap nombre negatiu
+    print("No hi ha cap nombre negatiu") # Imprimeix el missatge corresponent
 
 
